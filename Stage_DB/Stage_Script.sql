773d7ec95c2872edc57e60b9e5fd2187b4373e69
tgen0123 <100363182+tgen0123@users.noreply.github.com>
2024-11-22 17:41:13 +0700

    Update Stage_Script.sql

--- Stage_DB/Stage_Script.sql
+++ Stage_DB/Stage_Script.sql
@@ -6,15 +6,15 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[2Buscounties_STAGE](
-	[county] [nvarchar](50) NOT NULL,
-	[county_ascii] [nvarchar](50) NOT NULL,
-	[county_full] [nvarchar](50) NOT NULL,
-	[county_fips] [int] NOT NULL,
-	[state_id] [nvarchar](50) NOT NULL,
-	[state_name] [nvarchar](50) NOT NULL,
-	[lat] [float] NOT NULL,
-	[lng] [float] NOT NULL,
-	[population] [int] NOT NULL,
+	[county] [nvarchar](50),
+	[county_ascii] [nvarchar](50),
+	[county_full] [nvarchar](50),
+	[county_fips] [int],
+	[state_id] [nvarchar](50),
+	[state_name] [nvarchar](50),
+	[lat] [float],
+	[lng] [float],
+	[population] [int],
 ) ON [PRIMARY]
 GO
 
@@ -22,19 +22,19 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE TABLE [dbo].[aqi_2021_STAGE](
-	[State_Name] [nvarchar](50) NOT NULL,
-	[county_Name] [nvarchar](50) NOT NULL,
-	[State_Code] [int] NOT NULL,
-	[County_Code] [int] NOT NULL,
-	[Date] [date] NULL,
-	[AQI] [int] NOT NULL,
-	[Category] [nvarchar](50) NOT NULL,
-	[Defining_Parameter] [nvarchar](50) NOT NULL,
-	[Defining_Site] [nvarchar](50) NOT NULL,
-	[Number_of_Sites_Reporting] [int] NOT NULL,
-	[Created] [datetime] NOT NULL,
-	[Last_Updated] [datetime] NOT NULL,
+CREATE TABLE [dbo].[aqi_state_STAGE](
+	[State_Name] [nvarchar](50),
+	[county_Name] [nvarchar](50),
+	[State_Code] [int],
+	[County_Code] [int],
+	[Date] [date],
+	[AQI] [int],
+	[Category] [nvarchar](50),
+	[Defining_Parameter] [nvarchar](50),
+	[Defining_Site] [nvarchar](50),
+	[Number_of_Sites_Reporting] [int],
+	[Created] [datetime],
+	[Last_Updated] [datetime],
 ) ON [PRIMARY]
 GO
 
@@ -42,39 +42,18 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE TABLE [dbo].[aqi_2022_STAGE](
-	[State_Name] [nvarchar](50) NOT NULL,
-	[county_Name] [nvarchar](50) NOT NULL,
-	[State_Code] [int] NOT NULL,
-	[County_Code] [int] NOT NULL,
-	[Date] [date] NULL,
-	[AQI] [int] NOT NULL,
-	[Category] [nvarchar](50) NOT NULL,
-	[Defining_Parameter] [nvarchar](50) NOT NULL,
-	[Defining_Site] [nvarchar](50) NOT NULL,
-	[Number_of_Sites_Reporting] [int] NOT NULL,
-	[Created] [datetime] NOT NULL,
-	[Last_Updated] [datetime] NOT NULL,
+CREATE TABLE [dbo].[aqi_state_STAGE](
+	[State_Name] [nvarchar](50),
+	[county_Name] [nvarchar](50),
+	[State_Code] [int],
+	[County_Code] [int],
+	[Date] [date],
+	[AQI] [int],
+	[Category] [nvarchar](50),
+	[Defining_Parameter] [nvarchar](50),
+	[Defining_Site] [nvarchar](50),
+	[Number_of_Sites_Reporting] [int],
+	[Created] [datetime],
+	[Last_Updated] [datetime],
 ) ON [PRIMARY]
 GO
-
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [dbo].[aqi_2023_STAGE](
-	[State_Name] [nvarchar](50) NOT NULL,
-	[county_Name] [nvarchar](50) NOT NULL,
-	[State_Code] [int] NOT NULL,
-	[County_Code] [int] NOT NULL,
-	[Date] [date] NULL,
-	[AQI] [int] NOT NULL,
-	[Category] [nvarchar](50) NOT NULL,
-	[Defining_Parameter] [nvarchar](50) NOT NULL,
-	[Defining_Site] [nvarchar](50) NOT NULL,
-	[Number_of_Sites_Reporting] [int] NOT NULL,
-	[Created] [datetime] NOT NULL,
-	[Last_Updated] [datetime] NOT NULL,
-) ON [PRIMARY]
-GO
-
